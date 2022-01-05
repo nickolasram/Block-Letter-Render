f27457177154385577c721bad131e5d7f396942e
Nickolas <91910852+nickolasram@users.noreply.github.com>
2022-01-04 21:18:13 -0800

    Update test.js

--- Resources/scripts/test.js
+++ Resources/scripts/test.js
@@ -11,7 +11,7 @@ function getText() {
         let zester = lines[x]; //zester is a variable used for testing
         if (zester.length > lineLimit) { //starts new line if too many characters on one line
           if (!zester.charAt(lineLimit-lineRedux)==' '){
-		  if(zester.charAt(lineLimit-lineRedux-1)==' '){
+  	    if(zester.charAt(lineLimit-lineRedux-1)==' '){
               lines.splice(x,1,setCharAt(zester,lineLimit-lineRedux," "));
               lines.splice(x+1,0,getCharAt(zester,lineLimit-lineRedux));
             }
